a1e0d1cb341f3aa1fedf49e543f04623365ce44e
Ivan Lev <fanatos@mail.ru>
2024-05-15 18:10:26 +0500

    feat: add variables with tea count and prices

---
+++ src/variables/teas.ts
@@ -0,0 +1,15 @@
+import type { Teas } from '../types/teas';
+
+export const teas: Teas = [
+  { id: 1, name: 'Шу Гунтин Мэнхай', price: 14, count: 15 },
+  { id: 2, name: 'Шу Гунтин Линцан', price: 16, count: 15 },
+  { id: 3, name: 'Шэн Иу Гушу', price: 25, count: 15 },
+  { id: 4, name: 'Милансян ФХДЦ', price: 18, count: 15 },
+  { id: 5, name: 'Ци Лань', price: 12, count: 15 },
+  { id: 6, name: 'Те Гуаньинь', price: 14, count: 15 },
+  { id: 7, name: 'Габа Алишань', price: 22, count: 15 },
+  { id: 8, name: 'Дундин', price: 20, count: 15 },
+  { id: 9, name: 'Гушу Дянхун', price: 12, count: 15 },
+  { id: 10, name: 'Да Хун Пао', price: 14, count: 15 },
+  { id: 11, name: 'Баймудань', price: 25, count: 15 }
+];
