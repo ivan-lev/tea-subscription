930e2463513a34175f849ae526303fc2466a29e5
Ivan Lev <fanatos@mail.ru>
2024-05-15 16:02:37 +0500

    feat: add Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,12 @@
 import './Header.scss';
 
+import Logo from '../Logo/Logo';
+
 export default function Header() {
-  return <header className="header">Header here</header>;
+  return (
+    <header className="header">
+      <Logo />
+      <h1 className="header__title">Чайная подписка</h1>
+    </header>
+  );
 }
