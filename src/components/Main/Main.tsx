e996d27158bbbe627baba148b9ba53c47496cd06
Ivan Lev <fanatos@mail.ru>
2024-05-15 22:29:56 +0500

    feat: add Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,41 @@
 import './Main.scss';
 
+import { useState, useEffect } from 'react';
+
+import Row from '../Row/Row';
+
+import { teas } from '../../variables/teas';
+
+import { Tea } from '../../types/teas';
+
 export default function Main() {
-  return <main className="content">Main here</main>;
+  const [teaList, setTeaList] = useState(teas);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    calculateTotalSum();
+  }, [teaList]);
+
+  const calculateTotalSum = (): void => {
+    const total = teaList.reduce((accumulator: number, currentTea: Tea): number => {
+      return accumulator + currentTea.count * currentTea.price;
+    }, 0);
+    setTotalPrice(total + 120);
+  };
+
+  return (
+    <main className="content">
+      <div className="content__list">
+        <span>Название</span>
+        <span>Цена за гр</span>
+        <span></span>
+        <span>Кол-во</span>
+        <span></span>
+        {teaList.map(tea => (
+          <Row key={tea.id} tea={tea} teaList={teaList} setTeaList={setTeaList} />
+        ))}
+      </div>
+      Итого: {totalPrice <= 120 ? 0 : totalPrice}
+    </main>
+  );
 }
