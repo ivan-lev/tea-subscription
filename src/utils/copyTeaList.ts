42de6f054cbc91f6418940b741ed022470d94a74
Ivan Lev <fanatos@mail.ru>
2024-05-16 13:38:42 +0500

    feat: add copy to clipboard util

---
+++ src/utils/copyTeaList.ts
@@ -0,0 +1,25 @@
+import { Teas } from '../types/teas';
+
+export const copyTeaList = (teaList: Teas, teaCost: number, shippingCost: number) => {
+  if (navigator.clipboard) {
+    let teaListToString: string = '';
+
+    teaList.forEach(tea => {
+      const teaString: string = `${tea.id}. ${tea.name}: ${tea.count}гр на ${
+        tea.price * tea.count
+      } руб`;
+      teaListToString += teaString + '\n';
+    });
+
+    teaListToString += `Итого: ${teaCost + shippingCost}`;
+
+    navigator.clipboard
+      .writeText(teaListToString)
+      .then(() => alert('Done!'))
+      .catch(error => console.error(error));
+    return;
+  } else {
+    alert('Не получается скопировать на вашем устройстве, просто напишите мне и мы всё решим');
+    return;
+  }
+};
