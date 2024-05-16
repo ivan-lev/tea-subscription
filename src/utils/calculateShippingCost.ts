5412b41698a86488b567f85ccf6c060ce270fcac
Ivan Lev <fanatos@mail.ru>
2024-05-16 13:40:13 +0500

    feat: add shipping cost calculating util

---
+++ src/utils/calculateShippingCost.ts
@@ -0,0 +1,22 @@
+export const calculateShippingCost = (teaCost: number): number => {
+  if (teaCost >= 0 && teaCost < 1500) {
+    return 250;
+  }
+  if (teaCost >= 1500 && teaCost < 2000) {
+    return 225;
+  }
+  if (teaCost >= 2000 && teaCost < 2500) {
+    return 200;
+  }
+  if (teaCost >= 2500 && teaCost < 2880) {
+    return 175;
+  }
+  if (teaCost >= 2880 && teaCost < 3500) {
+    return 120;
+  }
+  if (teaCost >= 3500 && teaCost < 4000) {
+    return 100;
+  }
+  // if the cost of tea is more than 4000, the shipping will be free
+  return 0;
+};
