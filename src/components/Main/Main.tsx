b73663459ccd908f99bfe8c0c2e9e49633107546
Ivan Lev <fanatos@mail.ru>
2024-05-16 10:02:05 +0500

    feat/style: add copy tea list function and buttons

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 import Row from '../Row/Row';
 
 import { teas } from '../../variables/teas';
-
 import { Tea } from '../../types/teas';
 
 export default function Main() {
@@ -23,6 +22,30 @@ export default function Main() {
     setTotalPrice(total + 120);
   };
 
+  const copyList = (): void => {
+    if (navigator.clipboard) {
+      let teaListToString: string = '';
+
+      teaList.forEach(tea => {
+        const teaString: string = `${tea.id}. ${tea.name}: ${tea.count}гр на ${
+          tea.price * tea.count
+        } руб`;
+        teaListToString += teaString + '\n';
+      });
+
+      teaListToString += `Итого: ${totalPrice}`;
+
+      navigator.clipboard
+        .writeText(teaListToString)
+        .then(() => alert('Done!'))
+        .catch(error => console.error(error));
+      return;
+    } else {
+      alert('Не получается скопировать на вашем устройстве, просто напишите мне и мы всё решим');
+      return;
+    }
+  };
+
   return (
     <main className="content">
       <div className="content__list">
@@ -36,6 +59,18 @@ export default function Main() {
         ))}
       </div>
       Итого: {totalPrice <= 120 ? 0 : totalPrice}
+      <span className="content__buttons">
+        <button className="content__copy-button" onClick={copyList}>
+          Копировать список
+        </button>
+        <a
+          className="content__tg_write"
+          href="https://web.telegram.org/k/#@ivanlev"
+          target="_blank"
+        >
+          <span>Написать</span>
+        </a>
+      </span>
     </main>
   );
 }
