881153243d00eb6929861c645bb9b230bf7c4752
Ivan Lev <fanatos@mail.ru>
2024-05-22 18:45:17 +0500

    feat: provide store to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import './index.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
-import './index.scss';
+
+import { Provider } from 'react-redux';
+import store from './slices/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
