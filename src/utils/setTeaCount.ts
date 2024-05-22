1c3c3af6c8a52a22e645a8a11863322bf85c4e01
Ivan Lev <fanatos@mail.ru>
2024-05-22 18:47:40 +0500

    refactor: delete unused utils

--- src/utils/setTeaCount.ts
+++
@@ -1,7 +0,0 @@
-import type { Teas } from '../types/teas';
-
-export const setTeaCount = (countToSet: number, teas: Teas): Teas => {
-  return teas.map(tea => {
-    return { ...tea, count: countToSet };
-  });
-};
