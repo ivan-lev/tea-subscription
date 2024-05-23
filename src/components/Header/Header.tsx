54733c3eabe86aa95bd0bd673e21fd758094ae8a
Ivan Lev <fanatos@mail.ru>
2024-05-23 20:13:05 +0500

    feat: add info button in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,27 @@
 import './Header.scss';
 
+import { useState } from 'react';
+
 import Logo from '../Logo/Logo';
 
+import { HEADER_INFO } from '../../variables/variables';
+
 export default function Header() {
+  const [isDescriptionShowed, setIsDescriptionShowed] = useState<boolean>(false);
+
   return (
     <header className="header">
       <Logo />
       <h1 className="header__title">Чайная подписка</h1>
+
+      <div className="header__info">
+        <button
+          className="button button__info"
+          onClick={() => setIsDescriptionShowed(true)}
+          onMouseLeave={() => setIsDescriptionShowed(false)}
+        ></button>
+        {isDescriptionShowed && <p className="header__description">{HEADER_INFO}</p>}
+      </div>
     </header>
   );
 }
--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -2,3 +2,6 @@ export const VARIABLES = {
   MIN_WEIGHT: 5,
   DEFAULT_WEIGHT: 15
 };
+
+export const HEADER_INFO =
+  'Наведите курсор на название чая или тапните по нему, чтобы посмотреть информацию';
